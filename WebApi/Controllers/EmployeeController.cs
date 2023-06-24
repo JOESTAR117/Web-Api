234292f3bdeb1f99922f98fdc445ac8e9bd94029
Joestar117 <juniorrios1231@gmail.com>
2023-06-24 18:15:44 -0300

    feat: Download Photo created

--- WebApi/Controllers/EmployeeController.cs
+++ WebApi/Controllers/EmployeeController.cs
@@ -17,11 +17,30 @@ namespace WebApi.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult Add(EmployeeViewModel employeeView)
+		public IActionResult Add([FromForm] EmployeeViewModel employeeView)
 		{
-			var employee = new Employee(employeeView.Name, employeeView.Age, null);
+			var filePath = Path.Combine("Storage", employeeView.Photo.FileName);
+
+			using Stream fileStream = new FileStream(filePath, FileMode.Create);
+			employeeView.Photo.CopyTo(fileStream);
+
+			var employee = new Employee(employeeView.Name, employeeView.Age, filePath);
+
 			_employeeRepository.Add(employee);
+
 			return Ok(employee);
+		}
+
+		[HttpPost]
+		[Route("{id}/download")]
+
+		public IActionResult DownloadPhoto(int id)
+		{
+			var employee = _employeeRepository.Get(id);
+
+			var dataBytes = System.IO.File.ReadAllBytes(employee.photo);
+
+			return File(dataBytes, "image/png", "image/jpg");
 		}
 
 		[HttpGet]
