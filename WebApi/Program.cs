9705643dcdbc004a93391ec0e2cb34cc05ac78b5
Joestar117 <juniorrios1231@gmail.com>
2023-06-25 14:35:12 -0300

    finished: Cors configured

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -73,6 +73,16 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: "MyPolicy",
+		policy =>
+		{
+			policy.WithOrigins("http://localhost:8080")
+			.AllowAnyHeader()
+			.AllowAnyMethod();
+		});
+});
 
 var key = Encoding.ASCII.GetBytes(WebApi.Key.Secret);
 
@@ -115,6 +125,7 @@ else
 {
 	app.UseExceptionHandler("/error");
 }
+app.UseCors("MyPolice");
 
 app.UseHttpsRedirection();
 
